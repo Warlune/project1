152bea6c0c70876340838d1313fb798446232fb9
Warlune <Warlune1@gmail.com>
2014-10-07 11:10:18 -0500

    cleaned up code

--- project1.php
+++ project1.php
@@ -1,33 +1,73 @@
 <?php
 
-define('NUMBER', 100);
 
-$number = mt_rand(1, NUMBER);
+if($argc == 3) {
+	if(is_numeric($argv[1]) && (is_numeric($argv[2]))) {
+		
+		if ($argv[1] <= $argv[2]) {
+			$number = mt_rand($argv[1], $argv[2]);
+			
+		} elseif ($argv[1] >= $argv[2]) {
+			$number = mt_rand($argv[2], $argv[1]);
+		}
 
-echo exec('clear');
-
-fwrite(STDOUT, 'Please choose a number 1-100. ');
-
-$value = trim(fgets(STDIN));
-
-echo exec('clear');
-
-fwrite(STDOUT, 'Please guess if the number is higher or lower. ');
-
-$answer = strtolower(trim(fgets(STDIN)));
-
-echo exec('clear');
-
-if(($answer == 'lower') && ($value < $number)) {
-	fwrite(STDOUT , "correct $value is lower than $number\n");
-} elseif (($answer == 'higher') && ($value > $number)) {
-	fwrite(STDOUT , "correct $value is higher than $number\n");
-} elseif (($answer == 'lower') && ($value > $number)) {
-	fwrite(STDOUT , "Incorrect, $value is higher than $number\n");
-} elseif (($answer == 'higher') && ($value < $number)) {
-	fwrite(STDOUT , "Incorrect $value is lower than $number\n");
-} elseif (($answer == 'higher') || ($answer == 'lower') && ($value == $number)) {
-	fwrite(STDOUT , "Incorrect $value is equal to $number\n"); 
+	} else {
+		echo "you need to use integers\n";
+		exit(0);
+	}
 } else {
-	echo "im not sure what happened\n";
+	// output ask for lower number
+
+	// ask it for higher number
 }
+
+				
+echo exec('clear');
+
+fwrite(STDOUT, 'Would you like to play a game? ');
+
+$ans = strtolower(trim(fgets(STDIN)));
+
+echo exec('clear');
+
+if ($ans == 'yes') {
+	fwrite(STDOUT, 'Please choose a number 1-100. ');
+
+	$value = trim(fgets(STDIN));
+
+	echo exec('clear');
+
+	echo "creating a random number for X\n";
+
+	while($i < 7) {
+		echo '.';
+		sleep(1);
+		$i++;
+	}
+
+	echo exec('clear');
+
+	fwrite(STDOUT, "Please guess if $value is higher or lower than X. ");
+
+	$answer = strtolower(trim(fgets(STDIN)));
+
+	echo exec('clear');
+
+	if(($answer == 'lower') && ($value < $number)) {
+		fwrite(STDOUT , "correct $value is lower than $number\n");
+	} elseif (($answer == 'higher') && ($value > $number)) {
+		fwrite(STDOUT , "correct $value is higher than $number\n");
+	} elseif (($answer == 'lower') && ($value > $number)) {
+		fwrite(STDOUT , "Incorrect, $value is higher than $number\n");
+	} elseif (($answer == 'higher') && ($value < $number)) {
+		fwrite(STDOUT , "Incorrect $value is lower than $number\n");
+	} elseif (($answer == 'higher') || ($answer == 'lower') && ($value == $number)) {
+		fwrite(STDOUT , "Incorrect $value is equal to $number\n"); 
+	} else {
+		echo "Please enter a integer\n";
+	}
+} else {
+	echo "come back when you would like to play.\n";
+}
+
+	
